6d9c71acb17a5a72b693c5d052e7f4f7774a873e
tamile oliveira <tamile.oliveira@hotmail.com>
2025-03-25 18:54:34 -0300

    Creating Menu

--- code/Game.py
+++ code/Game.py
@@ -2,19 +2,17 @@ import pygame
 
 from code.Menu import Menu
 
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
-
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
--- code/Menu.py
+++ code/Menu.py
@@ -1,4 +1,13 @@
 import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+MENU_OPTION = ('LEVEL 1',
+               'SCORE',
+               'EXIT')
+
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
 
 
 class Menu:
@@ -8,6 +17,29 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
-        self.window.blit(source=self.surf, dest=self.rect)
-        pygame.display.flip()
-        pass
+        menu_option = 0
+        pygame.mixer_music.load('./assets/Menu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Tank", (229, 255, 204), ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", (229, 255, 204), ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], (178, 255, 102), ((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], (255, 255, 255), ((WIN_WIDTH / 2), 200 + 25 * i))
+
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
